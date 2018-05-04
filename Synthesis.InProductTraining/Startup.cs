16b35ce5b59e07cb9ccbd3baab75751691294718
Garrett Davis <gdavis@prysm.com>
2018-05-04 15:46:30 -0400

    CU-704 Add request telemetry to microservices tracking and upgrade Cosmos DB telemetry

--- Synthesis.InProductTraining/Startup.cs
+++ Synthesis.InProductTraining/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.Owin.Cors;
 using Microsoft.Owin.Extensions;
 using Owin;
+using Synthesis.ApplicationInsights.Owin;
 using Synthesis.Owin.Security;
 using Synthesis.InProductTrainingService.Owin;
 using Synthesis.Tracking.Web;
@@ -20,6 +21,7 @@ namespace Synthesis.InProductTrainingService
             app.UseAutofacLifetimeScopeInjector(InProductTrainingServiceBootstrapper.RootContainer);
 
             app.UseMiddlewareFromContainer<GlobalExceptionHandlerMiddleware>();
+            app.UseApplicationInsightsTracking();
             app.UseMiddlewareFromContainer<CorrelationScopeMiddleware>();
 
             // This middleware performs our authentication and populates the user principal.
