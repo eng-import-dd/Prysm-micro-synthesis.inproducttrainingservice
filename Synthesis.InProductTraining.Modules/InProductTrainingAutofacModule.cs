b1fdfa43fb768583f16e050e21031f0368e5f2a7
Stephan Nagy <snagy@prysm.com>
2020-05-07 00:12:30 -0400

    Wire up the implementation and the interfaces.  Service should work now.

---
+++ Synthesis.InProductTraining.Modules/InProductTrainingAutofacModule.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using Autofac;
+using Synthesis.InProductTrainingService.Controllers;
+using Synthesis.Nancy.Autofac.Module.Microservice;
+using Synthesis.PrincipalService.InternalApi.Api;
+using Module = Autofac.Module;
+
+namespace Synthesis.InProductTrainingService
+{
+    public class InProductTrainingAutofacModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterModule(new MicroserviceAutofacModule(ServiceInformation.ServiceName,
+                ServiceInformation.ServiceNameShort,
+                Assembly.GetAssembly(GetType()),
+                false));
+
+            // Apis
+            builder.RegisterType<UserApi>().As<IUserApi>();
+
+            // Controllers
+            builder.RegisterType<InProductTrainingController>().As<IInProductTrainingController>();
+        }
+    }
+}
---
+++ Synthesis.InProductTraining.Modules/ServiceInformation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Synthesis.InProductTrainingService
+{
+
+    public class ServiceInformation
+    {
+        public const string ServiceName = "Synthesis.InProductTrainingService";
+        public const string ServiceNameShort = "inproducttraining";
+    }
+}
